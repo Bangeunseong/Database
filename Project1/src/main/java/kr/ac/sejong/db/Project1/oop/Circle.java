bb59c55f7c887255730e717b52a6a2d7dd514b91
Bangeunseong <bangeunseong@naver.com>
2023-03-11 13:00:54 +0900

    Commit

--- Project1/src/main/java/kr/ac/sejong/db/Project1/oop/Circle.java
+++ Project1/src/main/java/kr/ac/sejong/db/Project1/oop/Circle.java
@@ -2,14 +2,15 @@ package kr.ac.sejong.db.Project1.oop;
 
 public class Circle {
 	//Field
-	private int radius, x, y;
+	private int radius; private Position p;
 	
 	//constructor
-	public Circle() {x = y = 0; radius = 1;}
-	public Circle(int x, int y, int radius) {this.x = x; this.y = y; this.radius = radius;}
+	public Circle() {p = new Position(); radius = 1;}
+	public Circle(int x, int y, int radius) {p = new Position(x,y); this.radius = radius;}
 	
 	//Method
-	public void setPosition(int x, int y) {this.x = x; this.y = y;}
+	public void setPosition(int x, int y) {p.setXY(x, y);}
+	public Position getPosition() {return p;}
 	public void setRadius(int radius) {this.radius = radius;}
 	public int getRadius() {return radius;}
 	public double getArea() {return Math.PI * radius * radius;}
