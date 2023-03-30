0e0e9f6caadc31653db6127a9dfacc61bda96ec1
Bangeunseong <bangeunseong@naver.com>
2023-03-30 17:13:16 +0900

    Calculator update

--- Project1/src/main/java/kr/ac/sejong/db/Project1/Calculator/Calculator.java
+++ Project1/src/main/java/kr/ac/sejong/db/Project1/Calculator/Calculator.java
@@ -1,7 +1,9 @@
 package kr.ac.sejong.db.Project1.Calculator;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,23 +11,34 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Calculator extends JFrame {
-	Container c; JPanel pal;
+	Container c; 
+	JPanel operand_Panel;
+	JPanel formula_Panel; JTextArea jArea;
 	private List<JButton> jbutton_operands = new ArrayList<>();
 	JButton[] jbutton_Operators = new JButton[10];
 	
 	void setButton() {
 		c = getContentPane();
-		pal = new JPanel();
-		pal.setBackground(Color.LIGHT_GRAY);
-		pal.setLayout(new GridLayout(3,3));
+		c.setLayout(new BorderLayout());
+		operand_Panel = new JPanel();
+		operand_Panel.setLayout(new GridLayout(3,3));
+		
+		formula_Panel = new JPanel();
+		formula_Panel.setBackground(Color.LIGHT_GRAY);
+		formula_Panel.setLayout(new FlowLayout());
+		
+		jArea = new JTextArea(5,30);
+		formula_Panel.add(jArea);
 		
 		for(int i = 1; i < 10; i++) {
 			jbutton_operands.add(new JButton(String.valueOf(i)));
 		}
-		jbutton_operands.stream().forEach(s->pal.add(s));
-		c.add(pal);
+		jbutton_operands.stream().forEach(s->operand_Panel.add(s));
+		c.add(formula_Panel, BorderLayout.NORTH);
+		c.add(operand_Panel, BorderLayout.CENTER);
 	}
 	
 	public Calculator() {
@@ -35,10 +48,11 @@ public class Calculator extends JFrame {
 		setButton();
 		setVisible(true);
 	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Calculator();
 	}
-
 }
